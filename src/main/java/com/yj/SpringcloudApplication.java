854aa9f25c7fbdcc2f5aaddcae20a51095ffb2a6
JYou <2503121281@qq.com>
2018-10-02 11:20:48 +0800

    springcloud学习

--- src/main/java/com/yj/SpringcloudApplication.java
+++ src/main/java/com/yj/SpringcloudApplication.java
@@ -2,11 +2,14 @@ package com.yj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringcloudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudApplication.class, args);
+		
 	}
 }
